1fd84c69c477a0642263f1edbb3967021227d316
MeryemBoukraa <100629819+MeryemBoukraa@users.noreply.github.com>
2022-11-08 17:48:56 +0100

    signin done

--- view/js/login.js
+++ view/js/login.js
@@ -1,68 +1,57 @@
-var firstname = document.getElementById("FirstN"),
-  lastname = document.getElementById("lastN"),
-  user = document.getElementById("user"),
-  email = document.getElementById("email"),
-  password = document.getElementById("password"),
-  confirmpassword = document.getElementById("confirmP"),
-  form = document.getElementById("form"),
-  errormsg = document.getElementsByClassName("error"),
-  suc_icon = document.getElementsByClassName("success-icon"),
-  fail_icon = document.getElementsByClassName("failure-icon");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (firstname.value === "") {
-    errormsg[0].innerHTML = "first name cannot be blank";
-    fail_icon[0].style.opacity = "1";
-    suc_icon[0].style.opacity = "0";
-  } else {
-    errormsg[0].innerHTML = "";
-    fail_icon[0].style.opacity = "0";
-    suc_icon[0].style.opacity = "1";
-  }
-  if (lastname.value === "") {
-    errormsg[1].innerHTML = "Last name cannot be blank";
-    fail_icon[1].style.opacity = "1";
-    suc_icon[1].style.opacity = "0";
-  } else {
-    errormsg[1].innerHTML = "";
-    fail_icon[1].style.opacity = "0";
-    suc_icon[1].style.opacity = "1";
-  }
 
-  if (user.value === "") {
-    errormsg[2].innerHTML = "Username cannot be blank";
-    fail_icon[2].style.opacity = "1";
-    suc_icon[2].style.opacity = "0";
-  } else {
-    errormsg[2].innerHTML = "";
-    fail_icon[2].style.opacity = "0";
-    suc_icon[2].style.opacity = "1";
-  }
-  if (email.value === "") {
-    errormsg[3].innerHTML = "email cannot be blank";
-    fail_icon[3].style.opacity = "1";
-    suc_icon[3].style.opacity = "0";
-  } else {
-    errormsg[3].innerHTML = "";
-    fail_icon[3].style.opacity = "0";
-    suc_icon[3].style.opacity = "1";
-  }
-  if (password.value === "") {
-    errormsg[4].innerHTML = "password cannot be blank";
-    fail_icon[4].style.opacity = "1";
-    suc_icon[4].style.opacity = "0";
-  } else {
-    errormsg[4].innerHTML = "";
-    fail_icon[4].style.opacity = "0";
-    suc_icon[4].style.opacity = "1";
-  }
-  if (confirmpassword.value === "") {
-    errormsg[5].innerHTML = "confirmation cannot be blank";
-    fail_icon[5].style.opacity = "1";
-    suc_icon[5].style.opacity = "0";
-  } else {
-    errormsg[5].innerHTML = "";
-    fail_icon[5].style.opacity = "0";
-    suc_icon[5].style.opacity = "1";
-  }
-});
+(function ($) {
+    "use strict";
+
+    
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
