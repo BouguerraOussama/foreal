fe31a00a7717adf8ab42072f3ca6d9638dd67704
mahdisoussi <100629819+MeryemBoukraa@users.noreply.github.com>
2022-11-16 19:30:13 +0100

    Update config.php

--- controller/config.php
+++ controller/config.php
@@ -1,24 +1,9 @@
 <?php
-class Config
-{
-  private static $pdo = NULL;
-  public static function getConnexion()
-  {
-    if (!isset(self::$pdo)) {
-      try {
-        $pdo = new PDO(
-          'mysql:host=localhost;dbname=onlytrades',
-          'root',
-          '',
-          [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-          ]
-        );
-      } catch (Exception $e) {
-        die('Erreur: ' . $e->getMessage());
-      }
-    }
-    return $pdo;
-  }
+
+$dsn = "mysql:host=localhost;dbname=onlytrades";
+
+try {
+  $pdo = new PDO($dsn, 'root', '');
+} catch (PDOException $e) {
+  echo $e->getMessage();
 }
