d69a5d44aa51bd6537773d2ecde91a83505387fe
meryemboukraa <100629819+MeryemBoukraa@users.noreply.github.com>
2022-11-24 05:38:18 +0100

    Revert "updates and fixes"
    This reverts commit 3dc83ddff22b4dc7bbf3be94fd84884c9d3cab58.

--- controller/userController.php
+++ controller/userController.php
@@ -1,7 +1,8 @@
 <?php session_start(); ?>
 <?php require("C:\\xampp\htdocs\\foreal\model\\config.php"); ?>
 <?php
-
+// connect to the database
+// $pdo = config::getConnexion();
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
@@ -14,6 +15,8 @@ if (isset($_POST['reg_user'])) {
     $lastName = $_POST['lastName'];
     $birthdate = $_POST['birthdate'];
 
+    // first check the database to make sure
+    // a user does not already exist with the same username and/or email
     $user_check_query = "SELECT * FROM users WHERE email = :email";
     $stmt2 = $pdo->prepare($user_check_query);
     $stmt2->execute([
@@ -24,7 +27,6 @@ if (isset($_POST['reg_user'])) {
         echo ("email already exists");
     } else {
         // $password = md5($password_1); //encrypt the password before saving in the database
-        $hash = password_hash($password, PASSWORD_BCRYPT, ['cost' => 10]);
         $query = "INSERT INTO users (username, first_name ,last_name, email, password ,birthdate,gender) VALUES (:username,:firstName,:lastName, :email, :password,:birthdate,:gender)";
         $stmt = $pdo->prepare($query);
         $stmt->execute([
@@ -32,7 +34,7 @@ if (isset($_POST['reg_user'])) {
             ':lastName' => $lastName,
             ':username' => $username,
             ':email' => $email,
-            ':password' => $hash,
+            ':password' => $password,
             ':birthdate' => $birthdate,
             ':gender' => $gender
         ]);
@@ -43,51 +45,35 @@ if (isset($_POST['reg_user'])) {
 
 // LOGIN USER
 if (isset($_POST['login_user'])) {
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
+    // receive all input values from the form
 
-    $sql = "SELECT * FROM users WHERE email = :email";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([
-        ':email' => $email
+    $email = $_POST['email'];
+    // $phone = $_POST['phone'];
+    $password = $_POST['password'];
+
+
+    // first check the database to make sure
+    // a user does not already exist with the same username and/or email
+    $user_check_query = "SELECT * FROM users WHERE email = :email AND password = :password";
+    $stmt2 = $pdo->prepare($user_check_query);
+    $stmt2->execute([
+        ':email' => $email,
+        ':password' => $password
+
     ]);
-    $count = $stmt->rowCount();
-    if ($count == 0) {
-        $error = "Wrong credentials!";
-    } else if ($count > 1) {
-        $error = "Wrong credentials!";
-    } else if ($count == 1) {
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        $user_password_hash = $user['password'];
-        $user_name = $user['username'];
-        $user_role = $user['role'];
-        $user_status = $user['status'];
-        $hash = password_hash($password, PASSWORD_BCRYPT, ['cost' => 10]);
-        if ($user_status == 0) {
-            $error_ban = "user Banned! <br> Please contact the administration for more details. ";
-        } else if (strcmp($hash, $password)) {
-            $success = "Sign in successful!";
-            $user_id = $user['id'];
-            $user_nickname = $user['username'];
-            $d_user_id = base64_encode($user_id);
-            $d_user_nickname = base64_encode($user_nickname);
-            // userid
-            setcookie('_uid_', $d_user_id, time() + 60 * 60 * 24, '/', '', '', true);
-            // user nickname
-            setcookie('_uiid_', $d_user_nickname, time() + 60 * 60 * 24, '/', '', '', true);
-            $_SESSION['user_name'] = $user_name;
-            $_SESSION['id'] = $user['id'];
-            $_SESSION['role'] = $user_role;
-            $_SESSION['login'] = 'success';
-            if ($user_role == 'ADMIN') {
-                header('Location: http://localhost/foreal/view/back.html');
-                die();
-            } else {
-                header('Location: http://localhost/foreal/view/');
-                die();
-            }
-        } else {
-            $error_password = "Wrong password!";
-        }
+    $userCount = $stmt2->rowCount();
+    if ($userCount == 0) { // if user exists
+        echo ("email doesn't exists");
+    } else {
+        // $password = md5($password_1); //encrypt the password before saving in the database
+        //  $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        // $user_password_hash = $user['user_password'];
+        // $user_name = $user['user_name'];
+        // $user_role = $user['user_role'];
+        // $user_status = $user['user_status'];
+        echo ("email doesn't dsqdqsdqsd");
+
+        header('Location: http://localhost/foreal/view/');
+        die();
     }
 }
